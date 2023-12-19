7a9b32b0c1d8b2c46a75fe982bd7256adce643ca
vitaliykharitonov <120383978+vitaliykharitonov@users.noreply.github.com>
2023-12-20 00:12:25 +0200

    homework lesson 7;

---
+++ src/main/java/HomeWorkLesson7/Exercise.java
@@ -0,0 +1,13 @@
+package HomeWorkLesson7;
+
+public class Exercise {
+    private String exerciseName;
+    private int repetitions;
+    private int duration;
+
+    public Exercise(String exerciseName, int repetitions, int duration) {
+        this.exerciseName = exerciseName;
+        this.repetitions = repetitions;
+        this.duration = duration;
+    }
+}
---
+++ src/main/java/HomeWorkLesson7/Pagination.java
@@ -0,0 +1,13 @@
+package HomeWorkLesson7;
+
+public class Pagination {
+    private int limit;
+    private int offset;
+    private int total;
+
+    public Pagination(int limit, int offset, int total) {
+        this.limit = limit;
+        this.offset = offset;
+        this.total = total;
+    }
+}
