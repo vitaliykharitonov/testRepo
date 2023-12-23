1483d83fc6d224c95403f0d21e0d7692184fcb75
vitaliykharitonov <120383978+vitaliykharitonov@users.noreply.github.com>
2023-12-23 12:58:07 +0200

    homework lesson 8;

--- src/main/java/HomeWorkLesson8/Main.java
+++ src/main/java/HomeWorkLesson8/Main.java
@@ -1,28 +1,25 @@
 package HomeWorkLesson8;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 
 public class Main {
     public static void main(String[] args) {
-        class MyException extends RuntimeException {
-            public MyException(String message) {
-                super(message);
-            }
-        }
-
+        String fileName = "example.txt";
         try {
-            FileReader reader = new FileReader("example.txt");
-            reader.read();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            System.out.println("FNFException is catched");
+            FileReader reader = new FileReader(fileName);
         } catch (IOException e) {
-            System.out.println("IOException is catched");
-        } catch (MyException ex) {
-            System.out.println("MyExeption is catched!");
+            e.printStackTrace();
+            throw new MyException("MyException is thrown!");
+        } finally {
+            System.out.println("Program is still working!");
         }
+    }
+}
+
+class MyException extends RuntimeException {
+    public MyException(String message) {
+        super(message);
     }
 }
